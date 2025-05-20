9fa467e090ea7153ac687262212dd2d6af87370a
Taeseong Choi <slimshady913@naver.com>
2025-05-21 00:20:17 +0900

    fix: 여러개 체크할 수 있도록 오류 수정

--- backend/movies/filters.py
+++ backend/movies/filters.py
@@ -1,13 +1,17 @@
-from django_filters.rest_framework import FilterSet, ModelMultipleChoiceFilter
+import django_filters
 from .models import Movie
 from ott.models import OTT
 
-class MovieFilter(FilterSet):
-    ott_services = ModelMultipleChoiceFilter(
-        field_name='ott_services',
-        queryset=OTT.objects.all()
+class NumberInFilter(django_filters.BaseInFilter, django_filters.NumberFilter):
+    pass
+
+class MovieFilter(django_filters.FilterSet):
+    ott_services = NumberInFilter(
+        field_name="ott_services__id", 
+        lookup_expr='in'
     )
+    title = django_filters.CharFilter(field_name='title', lookup_expr='icontains')
 
     class Meta:
         model = Movie
-        fields = ['ott_services']
+        fields = ['ott_services', 'title']
